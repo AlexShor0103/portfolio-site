f963a36f4baf4ad9288d67c56d9037133070e69d
AlexShor0103 <sasha.shor024@gmail.com>
2022-09-16 13:33:23 -0400

    add Resume, About, Projects page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import Home from './Pages/Home';
+import About from './Pages/About';
+import Resume from './Pages/Resume';
+import Projects from './Pages/Projects';
 import './App.css';
-import {HashRouter as Router, Routes, Route, Navigate, Link} from "react-router-dom";
+
+import {HashRouter as Router, Routes, Route, Link} from "react-router-dom";
 function App() {
   return (
     <Router>
@@ -8,8 +12,12 @@ function App() {
         Hi!
       </div>
       <Routes>
-        <Route path="/" element={<Navigate to="/portfolio-site" replace />} />
-        <Route path="/portfolio-site" element={<Home />} />
+        {/* <Route path="/" element={<Navigate to="/portfolio-site" replace />} /> */}
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About/>} />
+        <Route path="/Resume" element={<Resume />} />
+        <Route path="/Projects" element={<Projects />} />
+
       </Routes>
     </Router>
     
